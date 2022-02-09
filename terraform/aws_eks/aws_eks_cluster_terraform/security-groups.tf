a769f6478cb32504d6414412fa5cd4a124dca87b
maninikesh2008 <maninikesh2008@gmail.com>
2022-02-09 13:13:04 -0600

    Update security-groups.tf

--- terraform/aws_eks/aws_eks_cluster_terraform/security-groups.tf
+++ terraform/aws_eks/aws_eks_cluster_terraform/security-groups.tf
@@ -1,46 +1,44 @@
-
 resource "aws_security_group" "worker_group_mgmt_one" {
   name_prefix = "worker_group_mgmt_one"
   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port = var.ingress_port_from
-    to_port   = var.ingress_port_to
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-    ]
-  }
+   dynamic ingress {
+    for_each = var.ec2_ingress_ports_default
+    content {
+      from_port   = ingress.key
+      to_port     = ingress.key
+      cidr_blocks = ingress.value
+      protocol    = "tcp"
+    }
+   }
 }
 
 resource "aws_security_group" "worker_group_mgmt_two" {
   name_prefix = "worker_group_mgmt_two"
   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port = var.ingress_port_from
-    to_port   = var.ingress_port_to
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "192.168.0.0/16",
-    ]
-  }
+ dynamic ingress {
+    for_each = var.ec2_ingress_ports_default
+    content {
+      from_port   = ingress.key
+      to_port     = ingress.key
+      cidr_blocks = ingress.value
+      protocol    = "tcp"
+    }
+   }
 }
 
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port = var.ingress_port_from
-    to_port   = var.ingress_port_to
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-      "172.16.0.0/12",
-      "192.168.0.0/16",
-    ]
-  }
+  dynamic ingress {
+    for_each = var.ec2_ingress_ports_default
+    content {
+      from_port   = ingress.key
+      to_port     = ingress.key
+      cidr_blocks = ingress.value
+      protocol    = "tcp"
+    }
+   }
+}
